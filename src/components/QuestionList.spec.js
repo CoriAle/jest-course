f5a3ef43b758bfef222853f17887c6683bb5906d
corina.nimatuj <corina.nimatuj@backcountry.com>
2020-12-16 16:31:36 -0600

    Watching before and after functionality

---
+++ src/components/QuestionList.spec.js
@@ -0,0 +1,21 @@
+describe('The Question list Component', () => {
+	beforeEach(()=>{
+		console.log("Before each");
+	});
+	beforeAll(()=>{
+		console.log("Before all");
+	});
+	afterEach(()=>{
+		console.log("after each");
+	});
+	afterAll(()=>{
+		console.log("after all");
+	});
+	it('Should display a list item', () => {
+		console.log("first running");
+
+	});
+	it('Should display a list item', () => {
+		console.log("second running");
+	});
+});
