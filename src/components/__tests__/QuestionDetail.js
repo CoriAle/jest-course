0ff1c72223a5c0aaec30db0742fb4015a0230861
corina.nimatuj <corina.nimatuj@backcountry.com>
2020-12-21 13:04:55 -0600

    Testing with react-test-renderer

--- src/components/__tests__/QuestionDetail.js
+++ src/components/__tests__/QuestionDetail.js
@@ -1,5 +1,42 @@
+import { mapStateToProps, QuestionDetailDisplay } from '../QuestionDetail';
+import renderer from 'react-test-renderer';
+import React from 'react';
+
 describe('The Question Detail Component', () => {
-	it('Should not regress', () => {
+	describe('The container element', () => {
+		describe('MapStateToProps', () => {
+			it('should map state to props correctly', () => {
+				const sampleQuestion = {
+					question_id: 42,
+					body: "Space is big",
+				};
+
+				const appState = {
+					questions: [sampleQuestion]
+				}
+				const ownProps = {
+					question_id: 42,
+				}
+				const componentState = mapStateToProps(appState, ownProps);
+				expect(componentState).toEqual(sampleQuestion);
+			});
+		});
+	});
+
+	describe('The display element', () => {
+		it('should not regress', () => {
+			const tree = renderer.create(
+				<QuestionDetailDisplay
+					title="The meaning of life"
+					body="42"
+					answer_count={0}
+					tags={['Tag']}
+				/>
+			);
+			expect(tree.toJSON()).toMatchSnapshot();
+	
+		});
 
 	});
+
 });
